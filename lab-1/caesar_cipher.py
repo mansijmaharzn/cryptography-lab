72cf87f13a2ec21dc81e705cc77ddecb2801c2ab
Mansij Maharjan <mansijmaharzn@gmail.com>
2024-05-08 18:30:09 +0545

    add decipher

--- lab-1/caesar_cipher.py
+++ lab-1/caesar_cipher.py
@@ -17,7 +17,30 @@ def caesar_cipher(text, key):
     return encrypted_text
 
 
+def caesar_decipher(text, key):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha():  # Check if the character is a letter
+            # Determine if it's uppercase or lowercase and get its position in the alphabet
+            if char.isupper():
+                h = ord(char) - ord('A')
+            else:
+                h = ord(char) - ord('a')
+
+            # Applying the Caesar cipher decryption formula
+            decrypted_char = chr((h - key) % 26 + ord('A') if char.isupper() else (h - key) % 26 + ord('a'))
+            decrypted_text += decrypted_char
+        else:
+            # Not a letter? leave it unchanged
+            decrypted_text += char
+    return decrypted_text
+
+
 plaintext = input("Enter Plaintext: ")
 key = int(input("Enter Key: "))
+
 encrypted_text = caesar_cipher(plaintext, key)
 print("Encrypted text:", encrypted_text)
+
+decrypted_text = caesar_decipher(encrypted_text, key)
+print("Decrypted text:", decrypted_text)
